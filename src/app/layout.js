43885997e2821888c82b95b641a5e36124b00995
Bruno Rios <brunorios292@gmail.com>
2025-05-07 18:44:57 -0300

    0.1.8

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,43 @@
+"use client"
 import "./globals.css";
+import { useState } from 'react';
 
 export default function RootLayout({ children }) {
 
+    const [isOpen, setIsOpen] = useState(false);
+
+
     return (
         <html lang="en">
-            
-            <body>
-                
-                <header>
 
-                    
-                    <h1>Test</h1>
-                    <nav>
-                        <ul>
-                            <li><a href="#inicio">Inicio</a></li>
-                            <li><a href="#servicios">Servicios</a></li>
-                            <li><a href="#contacto">Contaco</a></li>
-                        </ul>
-                    </nav>
+            <body>
+
+                <header>
+                    <div className="name">
+                        <p className="primary-text">Bruno Ríos</p>
+                        <p className="secondary-text">Soriano Uruguay</p>
+                    </div>
+                    <div className="hamburgermenu">
+                        <button className="hamburgerbutton" onClick={() => setIsOpen(!isOpen)}>☰</button>
+                    </div>
+                    <div className="menu">
+                        <p className="primary-text">Home</p>
+                        <p className="primary-text">Proyects</p>
+                        <p className="primary-text">About Me</p>
+                        <p className="primary-text">Contact Me</p>
+                    </div>
+
                 </header>
 
-                
+                <div className={`${isOpen ? 'hamburgerdisplay' : 'hamburgerhidden'}`}>
+
+                    <p className="primary-text">Hola puta de mierda</p>
+                </div>
+
+
                 {children}
             </body>
         </html>
     );
 }
+
