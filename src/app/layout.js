a71d4c5910be75c54af8045f4bf7e95748bfc149
Bruno Rios <brunorios292@gmail.com>
2025-05-09 18:29:11 -0300

    0.1.10

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,6 +6,12 @@ export default function RootLayout({ children }) {
 
     const [isOpen, setIsOpen] = useState(false);
 
+    let isLoaded = false;
+
+    setTimeout(() => {
+        isLoaded = !isLoaded;
+
+    }, 1000);
 
     return (
         <html lang="en">
@@ -18,7 +24,8 @@ export default function RootLayout({ children }) {
                         <p className="secondary-text">Soriano Uruguay</p>
                     </div>
                     <div className="hamburgermenu">
-                        <button className={`${!isOpen ? 'hamburgerbutton primary-text' : 'hamburgerhidden'}`} onClick={() => setIsOpen(!isOpen)}>Menu</button>
+                        <button className={`${!isOpen ? 'showhamburgerbutton' : 'hidehamburgerbutton'} hamburgerbutton primary-text`} onClick={() => setIsOpen(!isOpen)}>Menu</button>
+
                     </div>
                     <div className="menu">
                         <p className="primary-text">Home</p>
@@ -28,16 +35,15 @@ export default function RootLayout({ children }) {
                     </div>
 
                 </header>
-
-                <div className={`${isOpen ? 'hamburgerdisplay' : 'hamburgerhidden'}`}>
-                    <button className="hamburgerbutton primary-text" onClick={() => setIsOpen(!isOpen)}>Close</button>
+                
+                <div className={`${isOpen ? 'hamburgerdisplay' : 'hamburgerhidden'} hamburgermenu`}>
+                    <button className={`${isOpen ? 'showhamburgerbutton' : 'hidehamburgerbutton'} hamburgerbutton primary-text`} onClick={() => setIsOpen(!isOpen)}>Close</button>
                     <p className="primary-text">Home</p>
                     <p className="primary-text">Proyects</p>
                     <p className="primary-text">About Me</p>
                     <p className="primary-text">Contact Me</p>
                 </div>
-
-
+                <div className="animationhidder hamburgerhidden"></div>
                 {children}
             </body>
         </html>
