bcd26f872827cb1f4861bc0aa6db0ed754cf9ece
Bruno Rios <brunorios292@gmail.com>
2025-05-13 20:39:23 -0300

    0.2.1

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,14 @@
+import styles from "./page.module.css";
+
 export default function HomePage() {
 
     return (
-        <main>
-            <p className="primary-text">Bruno Rios</p>
+        <main className={styles.main}>
+            <div className={styles.ocupation}>
+                <h1 className={`${styles.inline} primary-text`}>Bruno Rios</h1>
+                <h1 className={`${styles.inline + " " + styles.test} primary-text`}>asasa</h1>
+            </div>
+
         </main>
     );
 }
